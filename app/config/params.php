1452f9a94864bcb2f1b1e65476a8b71f203687d9
Laurent <fery_laurent@hotmail.com>
2024-05-16 14:39:09 +0200

    Intégration Templates + Partials

--- app/config/params.php
+++ app/config/params.php
@@ -1,6 +1,10 @@
 <?php
 // Params
 
+// Initialisation des zones dynamiques
+$title = '';
+$content = '';
+
 // Parametres de connexion a la DB
 define('DB_HOST', 'localhost:8889');
 define('DB_NAME', 'book_hunter_2023');
---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,11 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+      <!-- Aside -->
+      <aside class="w-full md:w-1/4 p-3">
+       <?php include_once '../app/views/templates/partials/_aside.php' ?>
+      </aside>
+
+      <!-- Main content -->
+      <main class="w-full md:w-3/4 p-3">
+       <?php echo $content;?>
+      </main>
+    </div>
