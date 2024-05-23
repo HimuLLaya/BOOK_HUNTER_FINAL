e9aa8fa38d41cd9f3b857ea388a562a6b1ff872c
Laurent <fery_laurent@hotmail.com>
2024-05-23 13:42:24 +0200

    Route par defaut: page home + Helpers.truncate

--- app/config/params.php
+++ app/config/params.php
@@ -7,6 +7,6 @@ $content = '';
 
 // Parametres de connexion a la DB
 define('DB_HOST', 'localhost:8889');
-define('DB_NAME', 'book_hunter_2023');
+define('DB_NAME', 'book_hunter_2024');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
--- app/routers/index.php
+++ app/routers/index.php
@@ -1,2 +1,11 @@
 <?php
 //router principal
+
+
+// Route par defaut
+// Patern: /
+// CTRL: pagesController
+// Action: homeAction
+
+include_once '../app/controllers/pagesController.php';
+\App\controllers\pagesController\homeAction($connexion);
