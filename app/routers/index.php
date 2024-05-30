10c7ea7d618c2fa9fb579d2d5fe4709828e997ab
Laurent <fery_laurent@hotmail.com>
2024-05-30 14:06:59 +0200

    Route authors.index

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,11 +1,19 @@
 <?php
 //router principal
 
+//route des authors
+//Pattern: /?authors
+//CTRL : authorsController
+//ACTION : indexAction
+if(isset($_GET['authors'])):
+    include_once '../app/controllers/authorsController.php';
+\App\controllers\authorsController\indexAction($connexion);
+
 //route des books
 //Pattern: /?books
 //CTRL : booksController
 //ACTION : indexAction
-if(isset($_GET['books'])):
+elseif(isset($_GET['books'])):
     include_once '../app/controllers/booksController.php';
 \App\controllers\booksController\indexAction($connexion);
 
---
+++ app/views/authors/index.php
@@ -0,0 +1,4 @@
+<ul>
+<h2 class="text-2xl font-bold mb-4">Authors</h2>
+<?php include '../app/views/authors/_index.php';?>
+</ul>
